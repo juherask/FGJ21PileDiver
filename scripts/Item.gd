7eed6c043129d9adc62f03cf0cc216b9e645e446
Jussi Rasku <jussi.rasku@jyu.fi>
2021-02-15 21:33:44 +0200

    Out of bounds items are returned to a random box.

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -8,11 +8,32 @@ export var unique_item_id = 0
 var is_item = true
 
 const GRAB_DISTANCE = 50
+const OUT_OF_BOUNDS_Y = 10000
 
 signal item_grabbed
 
+	
 func _ready():
 	unique_item_id = get_instance_id()
+
+func _process(delta):
+	if abs(position.y) > OUT_OF_BOUNDS_Y:
+		Rn.G.randomize()
+		
+		# Has left the world, put it back to the random box
+		var all_boxes = []
+		for potential_box_or_item in get_parent().get_children():
+			if "is_box" in potential_box_or_item:
+				all_boxes.append(potential_box_or_item)
+		if len(all_boxes)==0:
+			return
+		var chosen_box_idx = Rn.G.randi_range(0, len(all_boxes)-1)
+		all_boxes[chosen_box_idx].restock_item($ItemSprite.texture,
+		                                       item_color, item_type)
+		
+		# it has been restocked to random box
+		get_parent().remove_child(self)
+		self.queue_free()
 
 func _input(event):
 	if event is InputEventMouseButton or \
